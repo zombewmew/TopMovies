41896385a6d813b8482da179a5b9f9483f5a16e3
christina <blueberrrycake@gmail.com>
2020-01-14 14:18:55 +0300

    Initial Commit

---
+++ TopMoviesTests/TopMoviesTests.swift
@@ -0,0 +1,34 @@
+//
+//  TopMoviesTests.swift
+//  TopMoviesTests
+//
+//  Created by zombewmew on 14.01.2020.
+//  Copyright © 2020 Christina S. All rights reserved.
+//
+
+import XCTest
+@testable import TopMovies
+
+class TopMoviesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
